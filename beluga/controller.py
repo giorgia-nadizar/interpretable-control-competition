55646ef3034e5fc6a939ddd1f5622aaa1bff0827
lurovi <luigirovito2@gmail.com>
2026-02-12 17:09:32 +0100

    explicit type declaration of State and Action for controllers

--- beluga/controller.py
+++ beluga/controller.py
@@ -1,8 +1,5 @@
-from __future__ import annotations
-
-import numpy as np
-
-from skdecide.core import D
+import random
+from skd_domains.skd_base_domain import State, Action
 from skd_domains.skd_spddl_domain import SkdSPDDLDomain
 
 
@@ -11,18 +8,40 @@ class Controller:
         self._domain = domain
         self._action_space = self._domain.get_action_space()
         self._observation_space = self._domain.get_observation_space()
+        
+    def domain(self):
+        return self._domain
+    
+    def get_available_actions(self, observation: State) -> list[Action]:
+        return self._domain.get_applicable_actions(observation).get_elements()
+    
+    def action_space(self):
+        return self._action_space
+    
+    def observation_space(self):
+        return self._observation_space
 
-    def control(self, observation: D.T_agent[D.T_observation]) -> D.T_agent[D.T_concurrency[D.T_event]]: # type: ignore
+    def control(self, observation: State) -> Action:
         raise NotImplementedError
 
 
 class RandomController(Controller):
 
     def __init__(self, domain: SkdSPDDLDomain, seed: int) -> None:
-        super().__init__(domain)
-        self._rng = np.random.default_rng(seed)
+        super().__init__(domain=domain)
+        self._rng = random.Random(seed)
 
-    def control(self, observation: D.T_agent[D.T_observation]) -> D.T_agent[D.T_concurrency[D.T_event]]: # type: ignore
-        possible_actions = self._domain.get_applicable_actions(observation).get_elements()
+    def control(self, observation: State) -> Action:
+        possible_actions = self.get_available_actions(observation)
         sampled_action = self._rng.choice(possible_actions)
         return sampled_action
+    
+
+class MedianIndexController(Controller):
+    def __init__(self, domain: SkdSPDDLDomain) -> None:
+        super().__init__(domain=domain)
+
+    def control(self, observation: State) -> Action:
+        possible_actions = self.get_available_actions(observation)
+        median_index = len(possible_actions) // 2
+        return possible_actions[median_index]
--- beluga/simulation.py
+++ beluga/simulation.py
@@ -1,28 +1,12 @@
-#!/usr/bin/env python
-
 import json
 import os
 import time
 
-import argparse
-import sys
-import uuid
-
-from generator.configurations.default_configuration import (
-    DefaultProblemConfig,
-)
-from generator.configurations.configs import UnsolvabilityScenario
-
-# from beluga_lib.problem_def import BelugaProblemDecoder
 from beluga_lib.beluga_problem import BelugaProblemDecoder
-
-from skd_domains.skd_pddl_domain import SkdPDDLDomain
-from skd_domains.skd_ppddl_domain import SkdPPDDLDomain
 from skd_domains.skd_spddl_domain import SkdSPDDLDomain
 
-from generate_instance import ProbConfig, main as encode_json
-
 from controller import RandomController
+
 
 def main():
     seed = 42
@@ -33,7 +17,7 @@ def main():
     with open(problem_out, "r") as fp:
         inst = json.load(fp, cls=BelugaProblemDecoder)
 
-    domain = SkdSPDDLDomain(inst, problem_name, problem_folder, seed=seed) # type: ignore
+    domain = SkdSPDDLDomain(inst, problem_name, problem_folder, seed=seed, classic=True) # type: ignore
     action_space = domain.get_action_space()
     observation_space = domain.get_observation_space()
 
@@ -46,25 +30,27 @@ def main():
     
     start_time = time.time()
     
-    while not domain._is_terminal(s) and step < (
-        max_simulation_steps if max_simulation_steps else 100
-    ):
+    while not domain._is_terminal(s) and step < (max_simulation_steps if max_simulation_steps else 100):
         a = controller.control(s)
         print(f"\nApplying action: {a}")
         o = domain.step(a)
+        is_terminated = o.termination
         s = o.observation
         r = o.value.reward
-        c = o.value.cost # ignored if reward is used as the optimization criterion, better account for reward only
         print(f"\nCurrent state: {s}")
-        print(f"Reward: {r}, Cost: {c}")
+        print(f"Reward: {r}")
         step += 1
         total_reward += r
-    print(f"\nTotal reward: {total_reward}")
+
+        if is_terminated:
+            print("\nEpisode terminated.")
+            break
     domain.cleanup()
     
     end_time = time.time()
     print(f"Simulation completed in {end_time - start_time:.2f} seconds")
-    
+    print(f"\nTotal reward: {total_reward}")
+
 
 if __name__ == "__main__":
     main()
